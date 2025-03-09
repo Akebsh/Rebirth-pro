fb0f5a5eea9a8d70eb5022334dc9ef89dd0e7770
Akebsh <145180072+Akebsh@users.noreply.github.com>
2025-03-09 22:08:03 +0900

    덱 클릭시 드로우

--- src/lib/components/Deck.svelte
+++ src/lib/components/Deck.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
     import { CardMovement } from "$lib/engine/CardManager";
     import { deck_store } from "$lib/engine/CardStore";
+    import { get } from "svelte/store";
 
+    // 덱 클릭 시 실행될 함수
+    function handleDeckClick() {
+        CardMovement.drawCard();
+    }
 </script>
 
 <style>
@@ -13,6 +18,11 @@
         border-radius: 9px;
         perspective: 1000px;
         background-color: #4f4fce;
+        cursor: pointer; /* 클릭 가능하도록 커서 변경 */
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: relative;
     }
 
     .deck-counter {
@@ -34,6 +44,8 @@
 <div>
     <h3>Deck</h3>
 </div>
-<div class="deck">
+
+<!-- 덱 클릭 시 handleDeckClick 실행 -->
+<div class="deck" on:click={handleDeckClick}>
     <div class="deck-counter">{$deck_store.length}</div>
 </div>
